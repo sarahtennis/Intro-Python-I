45ed5a1bb823dbaec77e8494339a207dcd411be4
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-12 16:55:38 -0500

    completes file_io.py, creates bar.txt file

--- src/file_io.py
+++ src/file_io.py
@@ -9,6 +9,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+import os
+import sys
+
+def build_path(file_name):
+    return os.path.join(sys.path[0], file_name)
+
+with open(build_path("foo.txt"), "r") as foo:
+    text = foo.read()
+    print(text)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +25,5 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open(build_path("bar.txt"), "w") as bar:
+    bar.write("The drought had lasted now for ten million years, and the reign of the terrible lizards had long since ended.\nHere on the Equator, in the continent which would one day be known as Africa, the battle for existence had reached a new climax of ferocity, and the victor was not yet in sight.\nIn this barren and desiccated land, only the small or the swift or the fierce could flourish, or even hope to survive.")
